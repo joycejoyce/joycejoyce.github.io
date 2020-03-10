e075ead02080d9d958c6bd51c07635693496e5c8
Joyce <dorith1989@gmail.com>
2020-03-10 15:11:39 +0800

    Categorize source codes into: 1)type 2)tool 3)constant

---
+++ 2_sweet_home_daily/js/daily/src/tool/tool.js
@@ -0,0 +1,16 @@
+function Check() {}
+Check.lengthesEqual = function(array1, array2) {
+    return array1.length === array2.length;
+};
+
+function Assert() {}
+Assert.isTrue = function(statement) {
+    if(!statement) {
+        throw new Exception(ERR_ASSERT_FAIL);
+    }
+};
+
+export {
+    Check,
+    Assert
+};
